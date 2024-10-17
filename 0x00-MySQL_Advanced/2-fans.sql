c62704947661f61825a71ef00fa035884325cec4
Rania FATHALLAH <rania14fathallah@gmail.com>
2024-10-17 20:01:36 +0100

    0

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,6 @@
+--  A SQL script that ranks country origins of bands, ordered by the number of (non-unique) fans.
+SELECT origin, SUM(nb_fans) AS nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC;
+
